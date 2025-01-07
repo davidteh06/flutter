2df9dc0b0820987f6f941ceb0960584d37afada6
Shou Luan Teh- <davidteh@airasia.com>
2025-01-07 15:27:09 +0800

    Update: added staging link

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a5720ae34045a6bdbf2865951f430f6f",
+const RESOURCES = {"flutter_bootstrap.js": "300b3b0c61fa140502172f722b475c7c",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7",
 "index.html": "5216f2958ee3c3848a75867a246f20ba",
 "/": "5216f2958ee3c3848a75867a246f20ba",
-"main.dart.js": "388f710a4cc341f873dfa25c3c93527d",
+"main.dart.js": "b8ed9bf3bd94d61297019947f274bc1a",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
